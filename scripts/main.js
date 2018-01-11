53e32b5adb6fc69551eb3b1177685414515cee86
Hamzou <karouihamza@gmail.com>
2018-01-11 19:16:49 +0100

    projet google js

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+function vaEtVient(){
+		if(document.getElementById('invisible').style.display == 'none'){
+    	document.getElementById('invisible').style.display = 'block';
+      document.getElementById('boutonclic').style.display = 'none';
+  		}
+  		else {
+    	document.getElementById('invisible').style.display = 'none';
+		}
+}
+
+function countPopup()
+{
+setTimeout("Popup()",10000);
+return false;
+}
+function Popup()
+{
+window.alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please','_blank');
+}
+
+
+var myButton = document.querySelector('#monlien');
+myButton.onclick = function() {
+  var mySearch = document.querySelector('#leinput').value;
+if(mySearch !== "") {
+  document.querySelector('#monlien').href = "https://www.google.fr/#q=" +mySearch
+     	}
+     }
